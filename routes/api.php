3e38ca6a0e5fe7a029343f6da26db24da6e7af5b
alvaro220592 <alvaro220592@gmail.com>
2021-10-19 21:45:36 -0300

    configurando o jwt

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 //use App\Http\Controllers\Api\ProductController;
 
 Route::group(['prefix' => 'v1'], function(){
-    Route::resource('products', 'Api\ProductController');
-    Route::post('products/search/', 'Api\ProductController@search');
+    Route::group(['middleware' => 'jwt.auth'], function(){
+        Route::resource('products', 'Api\ProductController');
+        Route::post('products/search/', 'Api\ProductController@search');
+    });
 });
